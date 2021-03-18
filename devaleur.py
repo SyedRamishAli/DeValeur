248248bcf573b7bdc8b5dace9fc1442063a3bf67
SyedRamishAli <50567482+SyedRamishAli@users.noreply.github.com>
2021-03-18 15:12:14 -0400

    Add files via upload

---
+++ devaleur.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 15:07:58 2021
+
+@author: syedramishali
+"""
+#print(":XAcwdc")
+x = input("write something")
+
+
+x = int(x)
+print(x + 2)
